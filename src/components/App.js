490fcf8d62086b862630c9777351daffb50c6253
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-11-01 19:01:00 +0530

    Updated full code

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,54 @@
-import React from "react";
-import MarkdownEditor from "./MarkdownEditor";
-import './../styles/App.css';
+// import React from "react";
+// import MarkdownEditor from "./MarkdownEditor";
+// import './../styles/App.css';
 
 
-function App() {
-  return (
-    <div className="app">
-      <MarkdownEditor />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="app">
+//       <MarkdownEditor />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import '../styles/App.css'
+
+const App = () => {
+    const [markdownContent, setMarkdownContent] = useState("");
+    const [loading, setLoading] = useState(true);
+
+    const handleChange = (e) => {
+        setMarkdownContent(e.target.value);
+    }
+
+    const changeLoadingVar = () => {
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        setTimeout(changeLoadingVar, 3000);
+    }, []);
+
+    return (
+        <>
+            {
+                loading ? (
+                    <div>
+                        <h1>Loading...</h1>
+                    </div >
+                ) : (
+                    <div className="app">
+                        <textarea onChange={handleChange} />
+                        <ReactMarkdown className="preview" children={markdownContent} />
+                    </div>
+                )}
+        </>
+    )
+};
 
 export default App;
--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,34 +1,34 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const MarkdownEditor = () => {
-  const [text, setText] = useState("# Hello world");
-  const [preview, setPreview] = useState("");
+// const MarkdownEditor = () => {
+//   const [text, setText] = useState("# Hello world");
+//   const [preview, setPreview] = useState("");
 
-  useEffect(() => {
-    let html = text;
-    if (html.startsWith("# ")) {
-      html = "<h1>" + html.slice(2) + "</h1>";
-    }
+//   useEffect(() => {
+//     let html = text;
+//     if (html.startsWith("# ")) {
+//       html = "<h1>" + html.slice(2) + "</h1>";
+//     }
 
-    setPreview(html);
-  }, [text]);
+//     setPreview(html);
+//   }, [text]);
 
 
-  return (
-    <div className="editor-container">
-      <textarea
-        className="textarea"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      ></textarea>
+//   return (
+//     <div className="editor-container">
+//       <textarea
+//         className="textarea"
+//         value={text}
+//         onChange={(e) => setText(e.target.value)}
+//       ></textarea>
 
-      <div
-        className="preview"
-        dangerouslySetInnerHTML={{ __html: preview }}
-      ></div>
-    </div>
-  );
+//       <div
+//         className="preview"
+//         dangerouslySetInnerHTML={{ __html: preview }}
+//       ></div>
+//     </div>
+//   );
 
-};
+// };
 
-export default MarkdownEditor;
+// export default MarkdownEditor;
